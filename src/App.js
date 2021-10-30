f31df06803ff0ea2f0e9f8fe2a13f948745c64bb
jitu3105 <ghugejalaj@gmail.com>
2021-10-31 01:14:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
+import {useEffect,useState} from 'react'
+import axios from 'axios'
 
 function App() {
+  // EMPTY STATE FOR DATA 
+  const [data,setData]=useState({});
+
+  //STATE FOR CURRENT PAGE
+  const [page,setPage]=useState(1);
+
+
+  //FETCH THE DATA WHEN PAGE LOADS AND WHENEVER THE PAGE GETS CHANGED... 
+  useEffect(()=>{
+    axios.get(`https://reqres.in/api/users?page=${page}`).then(res=>{
+      if(page>=1 && page<=res.data.total_pages)
+      {setData(res);}
+      
+    })
+  },[page])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className={classes.App}>
+      <div className={classes.showcase}>
+
+        {/* ------------------   MAPPED ALL THE FETCHED DATA IN FORM OF CARDS ------------------------------ */}
+      {data && data.data && data.data.data.map((dta,index)=>
+      <div className={classes.card}key={dta.id}>
+        <div className={classes.avatarContainer}>
+          <img src={dta.avatar}/>
+        </div>
+        <div className={classes.cardText}>
+          <p>{dta.first_name}</p>
+          <p>{dta.last_name}</p>
+          <p>{dta.email}</p>
+        </div>
+      </div>
+        
+        )}
+      </div>
+      
+      
+      {/* -------------------- PAGE NAVIGATION BUTTON CODE--------------------------------- */}
+      <div className={classes.nav}>
+      <button onClick={(e)=>{setPage(page-1)}} className={classes.navbtn} disabled={data && !(data.data && page === data.data.total_pages)}>-</button>
+      <p className={classes.navpage}>{page}</p>
+      <button onClick={(e)=>{setPage(page+1)}} className={classes.navbtn} disabled={data && data.data && page === data.data.total_pages}>+</button>
+      </div>
     </div>
   );
 }
